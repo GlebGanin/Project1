f6fd8eb2561dc2851ae83b940b23110b3e6f328b
Gleb Ganin <GLEBGANIN@GMAIL.COM>
2018-07-12 19:06:14 -0700

    added index, js, css

---
+++ assets/js/logic.js
@@ -0,0 +1,37 @@
+
+// var config = {
+//     apiKey: "AIzaSyDGrsO49ay55PJB4fXtsQk31vIhWZ7wMKw",
+//     authDomain: "addchild-37391.firebaseapp.com",
+//     databaseURL: "https://addchild-37391.firebaseio.com",
+//     projectId: "addchild-37391",
+//     storageBucket: "addchild-37391.appspot.com",
+//     messagingSenderId: "892075381341"
+//   };
+  
+//   firebase.initializeApp(config);
+  
+//   var dataRef = firebase.database();
+
+//   $("#submitScore").on("click", function(event) {
+//     event.preventDefault();
+
+
+
+
+
+
+
+
+var time = 100
+var sheep = 15
+var health = 50
+
+
+
+var score = (time*10) + (sheep*10) + (health*5);
+
+console.log(score);
+
+
+
+//   })
